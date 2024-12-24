fda7e3f5001d7226e9f679c2c1b143c84da340ad
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-12-24 14:18:31 +0200

    fix: using

--- src/Telegram.DotNet.Platform.Receiving/Commands/Typed/TypedTelegramCommand.cs
+++ src/Telegram.DotNet.Platform.Receiving/Commands/Typed/TypedTelegramCommand.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.DotNet.Platform.Receiving.Commands.Execution;
 
 namespace Telegram.DotNet.Platform.Receiving.Commands.Typed;
 
