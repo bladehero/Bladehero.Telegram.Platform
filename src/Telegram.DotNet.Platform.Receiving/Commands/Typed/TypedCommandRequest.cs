10cfda07a2e583fbc59c55625b5f01e88661eed7
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2025-09-09 14:46:49 +0300

    fix: reflection bootstrapping

--- src/Telegram.DotNet.Platform.Receiving/Commands/Typed/TypedCommandRequest.cs
+++ src/Telegram.DotNet.Platform.Receiving/Commands/Typed/TypedCommandRequest.cs
@@ -5,10 +5,11 @@ namespace Telegram.DotNet.Platform.Receiving.Commands.Typed;
 public sealed class TypedCommandRequest<TPayload>
     where TPayload : class
 {
-    internal TypedCommandRequest(TPayload payload, ITelegramBotClient client)
+    internal TypedCommandRequest(int updateId, TPayload payload, ITelegramBotClient client)
     {
         Payload = payload;
         Client = client;
+        UpdateId = updateId;
     }
 
     public int UpdateId { get; set; }
--- src/Telegram.DotNet.Platform.Receiving/Commands/Typed/TypedTelegramCommand.cs
+++ src/Telegram.DotNet.Platform.Receiving/Commands/Typed/TypedTelegramCommand.cs
@@ -11,38 +11,17 @@ public abstract class TypedTelegramCommand : ITelegramCommand
 
     internal static readonly Dictionary<UpdateType, PropertyInfo> UpdateProperties = typeof(Update)
         .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty)
-        .ToDictionary(x => Enum.Parse<UpdateType>(x.Name), x => x);
+        .GroupJoin(
+            Enum.GetValues<UpdateType>(),
+            x => x.Name,
+            x => x.ToString(),
+            (property, updateTypes) => new { Property = property, UpdateTypes = updateTypes }
+        )
+        .Where(x => x.UpdateTypes.Any())
+        .ToDictionary(x => x.UpdateTypes.First(), x => x.Property);
 
     public virtual Task<bool> CanHandleAsync(CommandRequest request, CancellationToken token) =>
         Task.FromResult(request.Update.Type == Type);
 
     public abstract Task HandleAsync(CommandRequest request, CancellationToken token = default);
-}
-
-public abstract class TypedTelegramCommand<T> : TypedTelegramCommand
-    where T : class
-{
-    private T? _payload;
-
-    public override Task<bool> CanHandleAsync(CommandRequest request, CancellationToken token)
-    {
-        _payload = (T)UpdateProperties[request.Update.Type].GetValue(request)!;
-        return CanHandleAsync(request.Update.Id, _payload, token);
-    }
-
-    public sealed override Task HandleAsync(CommandRequest request, CancellationToken token = default)
-    {
-        if (_payload is null)
-        {
-            throw new InvalidOperationException(
-                "Payload of update was not initialized properly through the CanHandleAsync method"
-            );
-        }
-
-        return HandleAsync(request.Update.Id, _payload!, token);
-    }
-
-    protected abstract Task<bool> CanHandleAsync(int updateId, T payload, CancellationToken token = default);
-
-    protected abstract Task HandleAsync(int updateId, T payload, CancellationToken token = default);
 }
