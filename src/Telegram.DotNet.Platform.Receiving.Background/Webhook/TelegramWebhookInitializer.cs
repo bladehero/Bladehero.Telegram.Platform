67acc34d1d6732de520fc353b75a3255733912ba
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2025-09-19 18:10:27 +0300

    feat: make webhook idempotent

--- src/Telegram.DotNet.Platform.Receiving.Background/Webhook/TelegramWebhookInitializer.cs
+++ src/Telegram.DotNet.Platform.Receiving.Background/Webhook/TelegramWebhookInitializer.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Telegram.Bot;
 
@@ -6,18 +7,34 @@ namespace Telegram.DotNet.Platform.Receiving.Background.Webhook;
 
 internal sealed class TelegramWebhookInitializer(
     ITelegramBotClient client,
-    IOptions<TelegramWebhookConfiguration> options
+    IOptions<TelegramWebhookConfiguration> options,
+    ILogger<TelegramWebhookInitializer> logger
 ) : IHostedLifecycleService
 {
     public async Task StartingAsync(CancellationToken cancellationToken)
     {
         var configuration = options.Value;
-        await client.SetWebhook(
-            configuration.WebhookUri.AbsoluteUri,
-            allowedUpdates: configuration.AllowedUpdates,
-            dropPendingUpdates: configuration.DropPendingUpdates,
-            cancellationToken: cancellationToken
-        );
+        try
+        {
+            var webhook = await client.GetWebhookInfo(cancellationToken);
+            if (webhook.HasNoChangesBasedOn(configuration))
+            {
+                return;
+            }
+
+            await client.DeleteWebhook(cancellationToken: cancellationToken);
+
+            await client.SetWebhook(
+                configuration.WebhookUri.AbsoluteUri,
+                allowedUpdates: configuration.AllowedUpdates,
+                dropPendingUpdates: configuration.DropPendingUpdates,
+                cancellationToken: cancellationToken
+            );
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical(ex, "Failed to initialize webhook");
+        }
     }
 
     #region Ignored
